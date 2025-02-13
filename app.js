76ce45f85fdabce8b52655827b7f461c0027eb34
SatyamRattan <uvsatyamrattan@gmail.com>
2025-02-13 14:57:34 +0530

    second commit

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const app = express();
+const port = 3000;
 
 app.get('/',(req,res)=>{
     res.send("Hello World");
 });
 
-app.listen(8000,()=>{
-    console.log("Server is running on port 8000");
+app.listen(port,()=>{
+    console.log("Server is running on port 3000");
 })
