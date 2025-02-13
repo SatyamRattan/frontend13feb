ba88bd29782db8973d1d62206aa773210be40faf
SatyamRattan <uvsatyamrattan@gmail.com>
2025-02-13 15:03:28 +0530

    Second Commit port change

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require("express")
+const express = require("express");
 const app = express();
+
 const port = 3000;
 
-
-app.get('/',(req,res)=>{
-    res.send("Hello World");
+app.get("/", (req, res) => {
+    res.send("Hello World!");
 });
 
-app.listen(port,()=>{
-    console.log("Server is running on port 3000");
-})
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
